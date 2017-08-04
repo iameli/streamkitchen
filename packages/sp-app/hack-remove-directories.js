9401162307848bfa78094841c4db03b9ada3341d
Eli Mallon <eli@stream.place>
2017-08-04 14:41:05 -0700

    remove all directories

--- packages/sp-app/hack-remove-directories.js
+++
@@ -1,2 +0,0 @@
-// When we're installed from npm while building the app, it adds a "directories" key to my
-// package.json. Electrion-builder really hates that for some reason. So this file strips it out. idk idk idk idk idk
