4d1e703f3860b366745e6b002de79504d06385cb
Eli Mallon <eli@stream.place>
2017-08-05 12:21:56 -0700

    Build Mac on Mac, Windows on Linux

--- run/ci-build-app.sh
+++ run/ci-build-app.sh
@@ -7,11 +7,13 @@ set -o pipefail
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 npm install -g npm
-echo $CERTIFICATE_OSX_P12 | base64 --decode > /tmp/certificate.p12
-ls -alhs /tmp/certificate.p12
-security create-keychain -p mysecretpassword build.keychain
-security default-keychain -s build.keychain
-security unlock-keychain -p mysecretpassword build.keychain
-security import /tmp/certificate.p12 -P "" -k build.keychain -T /usr/bin/codesign
-security find-identity -v
+if [[ "$(uname)" == "Darwin" ]]; then
+  echo $CERTIFICATE_OSX_P12 | base64 --decode > /tmp/certificate.p12
+  ls -alhs /tmp/certificate.p12
+  security create-keychain -p mysecretpassword build.keychain
+  security default-keychain -s build.keychain
+  security unlock-keychain -p mysecretpassword build.keychain
+  security import /tmp/certificate.p12 -P "" -k build.keychain -T /usr/bin/codesign
+  security find-identity -v
+fi
 node "$ROOT/run/ci-build-app.js"
